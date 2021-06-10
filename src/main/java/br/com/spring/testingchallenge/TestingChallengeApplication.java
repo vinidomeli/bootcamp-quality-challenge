6e20375ebedb32ee28ca8b3b5bf8bb0af0894607
Vinicius Lima <vinicius.limafernandes@mercadolivre.com>
2021-06-10 13:06:56 -0300

    Application structure with spring

---
+++ src/main/java/br/com/spring/testingchallenge/TestingChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.testingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingChallengeApplication.class, args);
+	}
+
+}
